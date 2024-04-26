5dbe2b5c8056c5bc941bea554458212fefde5ac0
IvanLu <835498692@qq.com>
2024-04-26 11:03:37 +0800

    update openai

--- api/openai.ts
+++ api/openai.ts
@@ -1,3 +1,11 @@
-import {createOpenAICompact} from "../util/index.ts";
-
-export default createOpenAICompact("https://api.openai.com/");
+export default async function (req: Request) {
+    const url = new URL(req.url);
+    url.host = "api.openai.com";
+    url.pathname = url.pathname.substring(7);
+    return await fetch(new Request(url.toString(), {
+        headers: req.headers,
+        method: req.method,
+        body: req.body,
+        redirect: "follow",
+    }));
+}

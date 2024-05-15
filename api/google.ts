9a3e5a71c72120d26eb176f34f441e1d6394da30
IvanLu <835498692@qq.com>
2024-05-16 01:24:19 +0800

    Update google.ts

--- api/google.ts
+++ api/google.ts
@@ -2,12 +2,6 @@ import {jsonResponse, optionsResponse, replyResponse} from "../util/index.ts";
 import {BUNNY_API_TOKEN, BUNNY_PATHS} from "../config/index.ts";
 
 const GEMINI_API_KEY = Deno.env.get("GEMINI_API_KEY");
-const safetySettings = [
-    "HARM_CATEGORY_HATE_SPEECH",
-    "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "HARM_CATEGORY_HARASSMENT"
-].map((x) => ({category: x, threshold: "BLOCK_NONE"}));
 
 function makeToken(auth): string {
     const token = auth.startsWith("Bearer ") ? auth.substring(7) : auth;
@@ -35,7 +29,6 @@ function convertMessages(messages) {
             contents.push({role: m.role, parts});
         }
     }
-    console.log("CONTENTS", contents);
     return {contents, system_instruction};
 }
 
@@ -51,21 +44,17 @@ export default async (req: Request) => {
         const token = makeToken(req.headers.get("Authorization"));
         const body = await req.json();
         const {model, messages, max_tokens, top_k, temperature, stream} = body;
-        console.log("BODY", body);
         return replyResponse(model, stream, () => {
-            console.log("START GEMINI");
             return fetch(makeURL(model, token), {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    safetySettings,
                     ...convertMessages(messages),
                 }),
             }).then((res) => res.body.getReader()).catch((err) => console.log(err));
         }, (m) => {
-            console.log(m);
             const c = m?.candidates?.[0];
             return c?.content?.parts?.[0]?.text || "";
         });
